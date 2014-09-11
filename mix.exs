5aae2eab797d339f1fdd3ff27ae1832517531a6d
Jamie Winsor <jamie@vialstudios.com>
2014-09-11 12:41:11 -0700

    version bump 1.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HashRing.Mixfile do
   def project do
     [
       app: :hash_ring_ex,
-      version: "1.1.1",
+      version: "1.1.2",
       elixir: "~> 1.0.0 or ~> 0.15.1",
       description: description,
       package: package,

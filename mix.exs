22f1ab73a99e58d06dd60078e025763daf612892
Ankit Chandawala <ankit.chandawala@kayako.com>
2016-09-29 19:25:40 +0530

    Update to use elixir version 1.x.x

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule HashRing.Mixfile do
     [
       app: :hash_ring_ex,
       version: "1.1.2",
-      elixir: "~> 1.0.0 or ~> 0.15.1",
+      elixir: "~> 1.0 or ~> 0.15.1",
       description: description,
       package: package,
       deps: deps,
